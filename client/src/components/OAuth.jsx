6e9b836a0e61ba43dbad4120b83dc2e907fd4a36
RansaraHimaruth <ransara.20221894@iit.ac.lk>
2024-08-22 21:08:03 +0530

    file path fix

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,7 @@
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
-import { signInSuccess } from "../redux/userSlice";
+import { signInSuccess } from "../redux/user/userSlice";
 
 export default function OAuth() {
   const dispatch = useDispatch();
